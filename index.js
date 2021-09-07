e8b1fa64c181928d433cbdff4003a75386d68e75
MichelleEsca <89814190+MichelleEsca@users.noreply.github.com>
2021-09-07 17:20:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function masacorporal(){
+  var peso=pasefloat(document.getElementById("peso").value);
+  var altura=pasefloat(document.getElementById("altura").value);
+  var IMC;
+  IMC = (peso/(altura)altura);
+  document.getElementById('IMC').innerHTML=IMC;
+}
